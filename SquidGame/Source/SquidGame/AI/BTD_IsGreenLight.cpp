f59d6d9914a46858a3e0ff5b00caa8319cf525c9
Dev Das <devdastour@gmail.com>
2021-10-27 13:36:34 +0530

    Revert "AI Creatures Not Attacking Fixed!!!"
    This reverts commit 12d5f33adb794faeb3483c3d1a81f173f59ec344.

--- SquidGame/Source/SquidGame/AI/BTD_IsGreenLight.cpp
+++ SquidGame/Source/SquidGame/AI/BTD_IsGreenLight.cpp
@@ -14,9 +14,20 @@ UBTD_IsGreenLight::UBTD_IsGreenLight()
 
 bool UBTD_IsGreenLight::CalculateRawConditionValue(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) const
 {
-	UE_LOG(LogTemp, Warning, TEXT("Calling"))
+	bool bSuccess = Super::CalculateRawConditionValue(OwnerComp, NodeMemory);
+	if (!bSuccess) return false;
 
 	AAICRedLightGreenLight* RedLightGreenLightController = Cast<AAICRedLightGreenLight>(OwnerComp.GetAIOwner());
+	if (!RedLightGreenLightController) return false;
 
-	return RedLightGreenLightController && RedLightGreenLightController->IsGreenLight();
+	if (RedLightGreenLightController->IsGreenLight())
+	{
+		UE_LOG(LogTemp, Warning, TEXT("True"))
+	}
+	else
+	{
+		UE_LOG(LogTemp, Warning, TEXT("False"))
+	}
+
+	return RedLightGreenLightController->IsGreenLight();
 }
